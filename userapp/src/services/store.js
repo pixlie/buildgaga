bb37e0e1455360db1e3282ddd48fd9616360380b
Sumit Datta <sumitdatta@gmail.com>
2020-04-28 09:34:38 +0700

    (chore) Set Redux state to window for pre-rendered pages; (chore) Added Google Analytics

--- userapp/src/services/store.js
+++ userapp/src/services/store.js
@@ -5,8 +5,15 @@ import rootReducer from './reducers';
 
 
 export default initialState => {
+  // Grab the state from a global variable injected into the server-generated HTML
+  // https://github.com/stereobooster/react-snap#redux
+  const preloadedState = window.__PRELOADED_STATE__;
+
+  // Allow the passed state to be garbage-collected
+  delete window.__PRELOADED_STATE__;
+
   initialState =
-    JSON.parse(window.localStorage.getItem("cache.redux.state")) || initialState;
+    preloadedState || JSON.parse(window.localStorage.getItem("cache.redux.state")) || initialState;
 
   const store = createStore(
     rootReducer,
@@ -25,6 +32,12 @@ export default initialState => {
     const persist = {};
 
     window.localStorage.setItem("cache.redux.state", JSON.stringify(persist));
+
+    // This is for React/Redux hydrate to work on client side
+    // https://github.com/stereobooster/react-snap#redux
+    window.snapSaveState = () => ({
+      __PRELOADED_STATE__: store.getState()
+    });
   });
 
   return store;
